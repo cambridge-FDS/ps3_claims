10efdbaab39f59b5ee595db45652c676e849c86d
BenNorsk <benjamin.brueckner@bluewin.ch>
2024-11-15 14:34:53 +0000

    Feat/Test - Implemented the custom Winsorizer and wrote the test for it. All winsorizer tests pass.

--- tests/preprocessing/test_winsorizer.py
+++ tests/preprocessing/test_winsorizer.py
@@ -5,10 +5,24 @@ from ps3.preprocessing import Winsorizer
 
 # TODO: Test your implementation of a simple Winsorizer
 @pytest.mark.parametrize(
-    "lower_quantile, upper_quantile", [(0, 1), (0.05, 0.95), (0.5, 0.5)]
+    "lower_quantile, upper_quantile", [(0, 1), (0.05, 0.95), (0.5, 0.5), (0.01, 0.99)]
 )
 def test_winsorizer(lower_quantile, upper_quantile):
 
     X = np.random.normal(0, 1, 1000)
 
-    assert False
+    # Initialize the Winsorizer
+    winsorizer = Winsorizer(lower_quantile=lower_quantile, upper_quantile=upper_quantile)
+
+    # Fit the Winsorizer
+    winsorizer.fit(X)
+
+    # Transform the data
+    X_winsorized = winsorizer.transform(X)
+
+    # Check that the data is winsorized correctly
+    lower_bound = np.quantile(X, lower_quantile)
+    upper_bound = np.quantile(X, upper_quantile)
+    
+    assert np.all(X_winsorized >= lower_bound)
+    assert np.all(X_winsorized <= upper_bound)
